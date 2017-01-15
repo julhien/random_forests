42fffc38f420e8f97fc86e00b0932da4cc305964
mathildecaron31 <mathicaron@hotmail.fr>
2017-01-15 12:48:46 +0100

    breast

--- results_parallel.py
+++ results_parallel.py
@@ -15,10 +15,10 @@ from Forest_RI_2 import *
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-SK_LEARN = False
+#SK_LEARN = False
 FOREST_SIZE = 100
 L = 1
-NUMBER_ITER = 1
+NUMBER_ITER = 10
 
 
 def forest_grow(df):
@@ -70,61 +70,62 @@ def forest_grow(df):
     generalisation_error_one_tree = single_tree_error(X_t, Y_t, f_forests[index_best])
 
     return [adaboost_error, error_selection, error_single, generalisation_error_one_tree]
-
-for data_file in ["datasets/votes.txt", "datasets/vehicle.txt", "datasets/ecoli.txt"]:
-# for data_file in [
-#                  "datasets/image.txt","datasets/pima-indians-diabetes.txt","datasets/ionosphere.txt","datasets/sonar.txt","datasets/vehicle.txt",
-#                   "datasets/votes.txt","datasets/vowel.txt", "datasets/ecoli.txt","datasets/glass.txt", "datasets/german.txt"]:
-    file = open("result/results_test/results_Forest_RC_par_"+data_file[9:], "w")
-# file = open("result\\results_Forest_RI_ionosphere.txt", "w")
-# for data_file in ["datasets/sonar.txt"]:
-    print data_file
-    f = open(data_file, "r")
-
-    X = []
-
-    with open(data_file, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            X.append(row)
-    to_swap = ['datasets/votes.txt', 'datasets/image.txt']
-
-    if data_file in to_swap:
-        X = swap_first_last(X)
-    X = convert_to_float(X)
-
-
-    file.write(data_file[9:-4] + '\n')
-
-    df = pandas.DataFrame(X)
-    if data_file in ["datasets/glass.txt","datasets/ecoli.txt"]:
-        df = df.drop(df.columns[0], axis=1)
-        print(df)
-    if data_file in ["datasets/vowel.txt"]:
-        df = df.drop(df.columns[:3], axis=1)
-    # Number of feature selected at each node
-    #F = [1, int(np.log(len(df.T) - 1) / np.log(2) - 1)]
-    print(df)
-    # forest_grow(df)
-    pool = Pool(processes=6)
-    y_parallel = pool.map(forest_grow, [df for i in range(NUMBER_ITER)])
-    pool.close()
-    pool.join()
-    y_parallel = np.array(y_parallel)
-
-    # print(y_parallel)
-    adaboost_error = np.mean(y_parallel[:,0])
-    error_selection = np.mean(y_parallel[:,1])
-    error_single = np.mean(y_parallel[:,2])
-    error_one_tree = np.mean(y_parallel[:,3])
-    # print(error_selection_tab)
-    # ☼print(test_set_error)
-    file.write(
-        'Adaboost = ' + str(adaboost_error) + '\n' + 'Selection = ' + str(error_selection) + '\n' + 'Single = ' + str(
-            error_single) + '\n' + 'One Tree = ' + str(error_one_tree) + '\n\n')
-    print(
-        'Adaboost = ' + str(adaboost_error) + '\n' + 'Selection = ' + str(error_selection) + '\n' + 'Single = ' + str(
-            error_single) + '\n' + 'One Tree = ' + str(error_one_tree) + '\n\n')
-
-
-    file.close()
+for i in range(2):
+    SK_LEARN=bool(i)
+    #for data_file in ["datasets/votes.txt", "datasets/vehicle.txt", "datasets/ecoli.txt"]:
+    for data_file in ["datasets/liver.txt",
+                      "datasets/image.txt","datasets/pima-indians-diabetes.txt","datasets/ionosphere.txt","datasets/sonar.txt","datasets/vehicle.txt",
+                       "datasets/votes.txt","datasets/vowel.txt","datasets/glass.txt", "datasets/german.txt"]:
+        #file = open("result/results_test/results_Forest_RC_par_"+data_file[9:], "w")
+    # file = open("result\\results_Forest_RI_ionosphere.txt", "w")
+    # for data_file in ["datasets/sonar.txt"]:
+        file = open("result\\"+str(i)+"Forest_RI_"+data_file[9:-4]+".txt", "w")
+        print data_file
+        f = open(data_file, "r")
+    
+        X = []
+    
+        with open(data_file, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                X.append(row)
+        to_swap = ['datasets/votes.txt', 'datasets/image.txt']
+    
+        if data_file in to_swap:
+            X = swap_first_last(X)
+        X = convert_to_float(X)
+    
+    
+        file.write(data_file[9:-4] + '\n')
+    
+        df = pandas.DataFrame(X)
+        if data_file in ["datasets/glass.txt","datasets/ecoli.txt"]:
+            df = df.drop(df.columns[0], axis=1)
+        if data_file in ["datasets/vowel.txt"]:
+            df = df.drop(df.columns[:3], axis=1)
+        # Number of feature selected at each node
+        #F = [1, int(np.log(len(df.T) - 1) / np.log(2) - 1)]
+        #print(df)
+        #forest_grow(df)
+        pool = Pool(processes=6)
+        y_parallel = pool.map(forest_grow, [df for i in range(NUMBER_ITER)])
+        pool.close()
+        pool.join()
+        y_parallel = np.array(y_parallel)
+    
+        # print(y_parallel)
+        adaboost_error = np.mean(y_parallel[:,0])
+        error_selection = np.mean(y_parallel[:,1])
+        error_single = np.mean(y_parallel[:,2])
+        error_one_tree = np.mean(y_parallel[:,3])
+        # print(error_selection_tab)
+        # ☼print(test_set_error)
+        file.write(
+            'Adaboost = ' + str(adaboost_error) + '\n' + 'Selection = ' + str(error_selection) + '\n' + 'Single = ' + str(
+                error_single) + '\n' + 'One Tree = ' + str(error_one_tree) + '\n\n')
+        print(
+            'Adaboost = ' + str(adaboost_error) + '\n' + 'Selection = ' + str(error_selection) + '\n' + 'Single = ' + str(
+                error_single) + '\n' + 'One Tree = ' + str(error_one_tree) + '\n\n')
+    
+    
+        file.close()
